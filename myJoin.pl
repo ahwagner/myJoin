aa44b70c78a10ebeafb83bb3f67d986216515ce2
Alex H. Wagner <awagner@genome.wustl.edu>
2015-04-14 13:14:01 -0500

    revise myJoin for clarity and efficiency.

--- myJoin.pl
+++ myJoin.pl
@@ -5,65 +5,44 @@ use warnings;
 use Data::Dumper;
 use Getopt::Long;
 
+Getopt::Long::Configure('no_ignore_case');
+
 #declare global variables, hashes and arrays
 my $duplicate;
 my $unique;
-my $head;
+my $header_text;
 my $help;
-my $ph;
-my %gene1;
+my $header;
+my %line1;
 my @uniq;
 my @dup;
 
 #set input flags
-GetOptions ("ph" => \$ph,
+GetOptions ("H" => \$header,
             "unique" => \$unique,
             "duplicate" => \$duplicate,
-            "help" => \$help)or die ("Error in command line arguments -h for help\n");
+            "help" => \$help) or die ("Error in command line arguments -h for help\n");
 
 #return help flag
-if ($help){
-    help_me();
-}
+help_me() if ($help);
+
+#check for incorrect usage of -d and -u
+die "Error in command line arguments --h for help\n" if ($duplicate && $unique);
 
 #command line input
 foreach(@ARGV){
-	gene_hash1($_);
+	line_hash1($_);
 }
 
 #add hash keys to uniq and dup arrays
-foreach my $gene_name(sort keys %gene1){
-if ($gene1{$gene_name} == 1){
-    push(@uniq, $gene_name);
-    }else{
-        push(@dup, $gene_name);
-        @dup = sort(@dup);
-    }
+if ($header){
+    print "$header_text\n";
 }
 
-#confirm arays populated correctly
-#print Dumper(@uniq);
-#print Dumper(@dup);
-
-#combine unique and duplicate arrays for complete sorted list
-my @combined = (@uniq, @dup);
-@combined = sort(@combined);
-
-#print statements
-if ($help){
-    help_me();
-}
-elsif ($ph){
-    print "$head\n";
-}
-if (!$duplicate && !$unique){
-    array_print(\@combined);
-}elsif($duplicate && $unique){
-    die "Error in command line arguments --h for help\n";
-}elsif($duplicate){
-    array_print(\@dup);
-}elsif($unique){
-    array_print(\@uniq);
+foreach my $line(sort keys %line1){
+	print "$line\n" if ($unique && $line1{$line} == 1);
+	print "$line\n" if ($duplicate && $line1{$line} > 1);
+	print "$line\n" if (!$unique && !$duplicate);
 }
 
 exit;
@@ -71,36 +50,36 @@ exit;
 
 ###################################################
 
-#Use first observed line as header if flagged, populate hash with gene name as hash key.
-#Duplicate gene names removed keys cannot be duplicated in hash.
+#Use first observed line as header if flagged, populate hash with line name as hash key.
+#Duplicate line names removed keys cannot be duplicated in hash.
 #increment any names repeated more than once, indicates they are duplicates.
-sub gene_hash1{
+sub line_hash1{
 	my $input = shift;
-    open(FILE, $input);
-    if ($ph){
-    $head = <FILE>;
-    chomp $head;
+    open(FILE, $input) or die $!;
+    if ($header){
+    	$header_text = <FILE>;
+    	chomp $header_text;
     }
 	while (<FILE>){
 		chomp;
-		my @data = split("\t");
-		$gene1{$data[0]} += 1;
-            }
-	close FILE;
-}
-
-#access and print arrays
-sub array_print {
-    my @array = @{$_[0]};
-    foreach(@array){
-        print "$_\n";
+		$line1{$_} += 1;
     }
+	close FILE;
 }
 
 #sub routine to print help statement
 sub help_me {
+    print 
+"
+Returns a join of multiple files, similar to GNU join.
 
-    print "usage: myjoin [file1] [file2]\n-no command options returns unique list\n-ph pulls first line, uses it as a header\n-u returns list of unique names\n-d returns list of duplicate names\n-u -d not accepted at the same time\n";
+usage: myjoin [options] file1 file2 [file3...]
+  options:
+    -h, --help	this help text
+    -H		files contain a header line
+    -u		returns list of unique lines [mutually exclusive with -d]
+    -d		returns list of duplicate lines [mutually exclusive with -u]
+
+";
     exit;
-
 }

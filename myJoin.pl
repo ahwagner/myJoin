b6d7715ca29c6af82c29fa1d4acdd4a63c6aaa98
LMT337 <lmt337@gmail.com>
2015-06-12 15:57:34 -0500

    Check this out please, it is different from the other version

--- myJoin.pl
+++ myJoin.pl
@@ -9,25 +9,46 @@ Getopt::Long::Configure('no_ignore_case');
 
 #declare global variables, hashes and arrays
 my $duplicate;
+my $field;
+my $i = 0;
 my $unique;
+my $number = -1;
 my $header_text;
 my $help;
 my $header;
+my $ref;
 my %line1;
+my %header_lib;
+my %attribu;
+my %count;
+my @headers;
 my @uniq;
 my @dup;
+my @field;
+my @row;
 
 #set input flags
-GetOptions("H" => \$header,
+GetOptions(
+"H" => \$header,
 "unique" => \$unique,
-"duplicate" => \$duplicate,
+"number:i" => \$number,
+"field:s" => \$field,
 "help" => \$help) or die("Error in command line arguments -h for help\n");
+
+#process field argument
+@field = split(",", $field);
 
 #return help flag
 help_me() if ($help);
 
 #check for incorrect usage of -d and -u
-die "Error in command line arguments --h for help\n" if ($duplicate && $unique);
+die "Error in command line arguments --h for help\n" if ($number > -1 && $unique);
+
+#determine file number for n to be called without a value, file number will correspond to
+#lines present in every file
+if ($number == 0){
+    $number = scalar @ARGV;
+}
 
 #command line input
 foreach(@ARGV){
@@ -39,12 +60,26 @@ if ($header){
     print "$header_text\n";
 }
 
-foreach my $line(sort keys %line1){
+#print logic for command line options and
+foreach my $line (sort keys %line1){
+    
+    
+    print "$line\n" if ($number > -1 && $number <= $line1{$line}); # $number: command line (min num files for rep)
+    # $line1{$line}: num of files line observed in
     print "$line\n" if ($unique && $line1{$line} == 1);
-    print "$line\n" if ($duplicate && $line1{$line} > 1);
-    print "$line\n" if (!$unique &&  !$duplicate);
+    print "$line\n" if (!$unique && $number == -1);
+    
 }
 
+
+#print Dumper(\@aoa);
+#print "Hash line1=";
+#print Dumper(\%line1);
+print Dumper(\%header_lib);
+print Dumper(\@field);
+#print Dumper(\@headers);
+#print Dumper(\@row);
+print Dumper(\%attribu);
 exit;
 
 ###################################################
@@ -52,22 +87,51 @@ exit;
 #Duplicate line names removed, keys can not be duplicated in hash.
 #increment any names repeated more than once, indicates they are duplicates.
 
+#subroutine to find duplicates within and between files
 sub line_hash1{
+    my %observed;
     my $input = shift;
     open(FILE, $input) or die $!;
     if ($header){
         $header_text = <FILE>;
         chomp $header_text;
+        @headers = split("\t", $header_text);
+        %header_lib = map {$headers[$_] => $_} 0..$#headers;
     }
+#    print "reading $input\n";
     while (<FILE>){
-        chomp;
-        $line1{$_} += 1;
+        @row = split("\t");
+#        map {print "my row $_\n";} @row;
+        my $key = '';
+        foreach my $k (@field){
+            $ref = $header_lib{$k};
+            if (exists $row[$ref]){
+            $key .=  $row[$ref];
+#           map {print "my row: $_\n";} @row;
+            }else{
+                die "input file contains empty fields";
+                }
+#           print "my key: $key\n";
+
+           
+            }
+        #This field
+        foreach my $h (keys %header_lib){
+            foreach my $v (@field){
+            if ($h ne $v){
+            my $i = $header_lib{$h};
+            $attribu{$key}{$h} = $row[$i];
+                }
+            }
+        }
+
     }
     close FILE;
 }
 
 
 
+#subroutine returns correct file usage
 sub help_me {
     print
     "
@@ -78,10 +142,9 @@ options:
     -H  files contain a header line
     -u  returns list of unique lines[mutually exclusive with -d]
     -n  returns list of duplicate lines in all files,  [mutually exclusive with -u]
-        n=1,2,[3...] minimum number of files line must be present in to output\n
-
+    n=1,2,[3...] minimum number of files line must be present in to output\n
     
     ";
-    exit;
 }
 
+exit;
